50cac33309bba2af69c66c2ac3667f8eba2f8a5c
Vinod Gullipalli <vinod.g@doodleblue.com>
2023-04-27 23:48:47 +0530

    latest

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [popup, setPopUp] = useState(false);
+  const handleClickOpen = () => {
+    console.log(":::::>", popup);
+    setPopUp(!popup);
+  };
+  const handleClickClose = (value) => {
+    console.log("======>>>", value);
+    setPopUp(false);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleClickOpen}> PopUp</button>
+      <div>
+        {popup ? (
+          <div className="main">
+            <div className="popup">
+              <div className="popup-header">
+                <h1>Pop Up</h1>
+                <h1 onClick={handleClickClose}>x</h1>
+              </div>
+              <div>
+                <p>This is react popup!!....</p>
+              </div>
+            </div>
+          </div>
+        ) : (
+          ""
+        )}
+      </div>
     </div>
   );
 }
